4eb0218849b8ff8758f39e43f65221361c96b52a
arjunagi <karthik.arjunagi@gmail.com>
2016-05-31 23:06:54 -0400

    Correction made
    The check to add possible choices to array was wrong. Was adding the
    already selected choices instead of the available ones.

--- ticTacToe/ComputerWith160IQ.java
+++ ticTacToe/ComputerWith160IQ.java
@@ -1,15 +1,18 @@
 package ticTacToe;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ComputerWith160IQ {
 	
 	public int pickSquare(TicTacToe game) {
 		ArrayList<Integer> possibleSquares = new ArrayList<Integer>();
 		for(int i=0; i<game.board.length; i++){
-			if(game.board[i] != game.initialToken)
+			if(game.board[i] == game.initialToken)
 				possibleSquares.add(i+1); // The board indexing is from 1 to 9 even for the computer.
 		}
 		Random rand = new Random();
+		int choice = possibleSquares.get(rand.nextInt(possibleSquares.size()));
+		return choice;
 	}
 }
